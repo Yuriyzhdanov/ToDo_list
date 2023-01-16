05a4395e577e6a6f72aa3b30232eac4bb76706fd
Yuriyzhdanov <yuuriyzhdanov@gmail.com>
2023-01-16 20:29:47 +0200

    Fetch (GET) method added to renderTask

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import isValidTextFields from "./isValidTextFields.js";
 
-
-
 const form = document.querySelector("#form");
 const titleInput = document.querySelector("#taskInput");
 const addButton = document.querySelector("#add");
@@ -16,26 +14,27 @@ tasksList.addEventListener("click", deleteTask);
 resetAll.addEventListener("click", deleteAllTasks);
 
 
-//  fetch('http://localhost:3000/tasks')
-// .then(res =>  res.json())
-// .then(data =>
-//    document.querySelector("#tasksList").innerHTML = 
-//    JSON.stringify(data));
+
+//Post відправка даних
+// Put - обмін Get - отримати Delete -видалення 
 
 
-const tasks = [
-   {
-      id: 122345,
-    title:"test1",
-    description: "test2",
-    checked: false,
-   }
-];
+// console.log('JSON.stringify(data))',JSON.stringify(data));
+
+// JSON.parse(data);
+// let tasks = [
+//    {
+//       id: 122345,
+//     title:"test1",
+//     description: "test2",
+//     checked: false,
+//    }
+// ];
 
 renderTask();
 
-
 function addTask(event) {
+   //POST 
   event.preventDefault();
 
   const newTask = {
@@ -44,7 +43,7 @@ function addTask(event) {
     description: descriptionInput.value,
     checked: false,
   };
-console.log(newTask);
+  //   console.log(newTask);
 
   tasks.push(newTask);
 
@@ -54,37 +53,16 @@ console.log(newTask);
   descriptionInput.value = "";
 }
 
-function renderTask() {
-   tasksList.innerHTML="";
-   tasks.forEach(function(task){
-   const renderHTML = `
-  <li id="${task.id}" class="list-item" >
-       <div class="list-item-main">
-             <a href="#" class="main-icons">
-             <i class="bi-check-circle"></i>
-             <i class="bi-circle" data-action="done" id="checked"></i>
-             <i class="bi-pen" data-action="edit" id="edit"></i>
-             <i class="bi-trash3" data-action="delete"></i>
-             </a>
-         <input type="text"
-   placeholder="empty" class="list-item-task" id="taskInput" value="${task.title}" readonly >
-         <input type="text"
-     placeholder="empty" class="list-item-description" id="taskDescription" value="${task.description}" readonly >
-       </div>
-  </li>`;
-  tasksList.innerHTML += renderHTML;
- })
-};
-
 function doneTask(event) {
+  //PUT 
   if (event.target.dataset.action !== "done") return;
   const parentNode = event.target.closest(".list-item");
 
   const id = Number(parentNode.id);
   const currentTask = tasks.find((task) => task.id === id); //obj
-  
-    currentTask.checked = !currentTask.checked;
-  
+
+  currentTask.checked = !currentTask.checked;
+
   const taskTitle = parentNode.querySelector("#taskInput");
   taskTitle.classList.toggle("list-item-task--done");
 
@@ -96,46 +74,47 @@ function doneTask(event) {
 }
 
 function editTask(event) {
+//PUT
   if (event.target.dataset.action === "edit") {
-
     const parentNode = event.target.closest(".list-item");
     const inputEdit = parentNode.querySelector("#taskInput");
 
     inputEdit.toggleAttribute("readonly");
     inputEdit.classList.toggle("checkEdit");
 
-   const descriptionEdit = parentNode.querySelector("#taskDescription");
+    const descriptionEdit = parentNode.querySelector("#taskDescription");
 
     descriptionEdit.toggleAttribute("readonly");
     descriptionEdit.classList.toggle("checkEdit");
 
-    parentNode.querySelector(".list-item-task").value
-    parentNode.querySelector(".list-item-description").value
+    parentNode.querySelector(".list-item-task").value;
+    parentNode.querySelector(".list-item-description").value;
 
-    tasks.forEach(element => {
+    tasks.forEach((element) => {
       console.log(element);
       if (parseInt(parentNode.id) === element.id) {
-          element.title = parentNode.querySelector('.list-item-task').value;
-          element.description = parentNode.querySelector('.list-item-description').value;
-          element.checked = parentNode.querySelectorAll('.bi-check-circle')[1] != undefined;
+        element.title = parentNode.querySelector(".list-item-task").value;
+        element.description = parentNode.querySelector(
+          ".list-item-description"
+        ).value;
+        element.checked =
+          parentNode.querySelectorAll(".bi-check-circle")[1] != undefined;
       }
-      console.log('tasks', tasks);
-      
-  });
-  //   for (let i = 0; i < tasks.length; i++) {
-  //     if (parseInt(parentNode.id) === tasks[i].id) {
-  //         tasks[i].title = parentNode.querySelector('.list-item-task').value;
-  //         tasks[i].description = parentNode.querySelector('.list-item-description').value;
-  //         tasks[i].checked = parentNode.querySelectorAll('.bi-check-circle')[1] != undefined;
-  //         console.log("tseg");
-  //     }
-  // }
+      // console.log("tasks", tasks);
+    });
+    //   for (let i = 0; i < tasks.length; i++) {
+    //     if (parseInt(parentNode.id) === tasks[i].id) {
+    //         tasks[i].title = parentNode.querySelector('.list-item-task').value;
+    //         tasks[i].description = parentNode.querySelector('.list-item-description').value;
+    //         tasks[i].checked = parentNode.querySelectorAll('.bi-check-circle')[1] != undefined;
+    //         console.log("tseg");
+    //     }
+    // }
   }
-
-
 }
 
 function deleteTask(event) {
+   //DELETE
   if (event.target.dataset.action !== "delete") return;
   const parentNode = event.target.closest(".list-item");
   const id = Number(parentNode.id);
@@ -147,6 +126,45 @@ function deleteTask(event) {
 }
 
 function deleteAllTasks() {
+   //DELETE
   tasksList.innerHTML = "";
   tasks = [];
+}
+
+function renderTask(tasks) {
+   //GET method
+   async function getResponse() {
+      const response = await fetch(
+         'http://localhost:3000/tasks',
+         {
+            method: 'GET',
+         }
+      );
+      const tasks = await response.json(); // Extracting data as a JSON Object from the response
+      
+      tasksList.innerHTML = "";
+      tasks.forEach(function (task) {
+         const renderHTML = `
+           <li id="${task.id}" class="list-item" >
+                 <div class="list-item-main">
+                       <a href="#" class="main-icons">
+                       <i class="bi-check-circle"></i>
+                       <i class="bi-circle" data-action="done" id="checked"></i>
+                       <i class="bi-pen" data-action="edit" id="edit"></i>
+                       <i class="bi-trash3" data-action="delete"></i>
+                       </a>
+                   <input type="text"
+             placeholder="empty" class="list-item-task" id="taskInput" value="${task.title}" readonly >
+                   <input type="text"
+               placeholder="empty" class="list-item-description" id="taskDescription" value="${task.description}" readonly >
+                 </div>
+           </li>`;
+         tasksList.innerHTML += renderHTML;
+       });
+   }
+ getResponse();
+ 
+  
+  
+
 }
